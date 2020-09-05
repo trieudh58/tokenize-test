4b3de59a20411b7f9d39504baa3c79d4c6763d0f
trieudh58 <dangtrieu25@gmail.com>
2020-09-05 20:49:45 +0700

    add configurable ws protocol

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const host = process.env.HOST || '127.0.0.1'
+const wsPrc = process.env.WS_PRC || 'ws'
 
 app.set('view engine', 'pug')
 app.use(express.static('public'))
@@ -14,14 +15,14 @@ app.get('/', (req, res) => {
 app.get('/part1', (req, res) => {
   res.render('partials/market', {
     title: 'Part 1',
-    wsUrl: `ws://${host}:9443/mock`,
+    wsUrl: `${wsPrc}://${host}:9443/mock`,
   })
 })
 
 app.get('/part2', (req, res) => {
   res.render('partials/market', {
     title: 'Part 2',
-    wsUrl: `ws://${host}:9443/real`,
+    wsUrl: `${wsPrc}://${host}:9443/real`,
   })
 })
 
